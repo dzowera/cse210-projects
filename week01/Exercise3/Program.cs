d7efa431e16f67403489613cb6ba700b72d235e0
dzowera <idzowera@byupathway.edu>
2025-03-05 19:10:37 +0200

    eya

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,10 +1,20 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine();
+
+        string response = "eya";
+
+        while (response == "eya")
+        {
+            Console.Write("Ukufuna usewereso?: ");
+            response = Console.ReadLine();
+        }
 
     }
 }
