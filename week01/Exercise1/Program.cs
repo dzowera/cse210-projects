0f1fea057c5ba0450566b99f3d8e7a140ca5617b
dzowera <idzowera@byupathway.edu>
2025-03-05 19:00:34 +0200

    to week 3

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // Exercise 1
+        // I put only Write so the input is on the same line
+         
+        Console.Write("What is your name?: ");
+        string f_name = Console.ReadLine();  
+
+        Console.Write("What is your last name?: ");
+        string l_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,88 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // Below is an empty line for output readability 
+        Console.WriteLine(); 
+
+        // Setting up variables
+        Console.Write("Please enter your grade: ");
+        int score = int.Parse(Console.ReadLine());
+        string grade;
+
+        if (score >= 90) 
+    
+        {
+            grade = "A";
+
+            if (score % 10 <= 3)
+            {
+                grade += "-";
+            }
+        }
+
+        else if (score >= 80 && score < 90) 
+        
+        {
+            grade = "B";
+
+            if (score % 10 >= 7)
+            {
+                grade += "+";
+            } 
+
+            if (score % 10 <= 3)
+            {
+                grade += "-";
+            }
+        }
+
+        else if (score >= 70 && score < 80)
+        {
+            grade = "C";
+
+            if (score % 10 >= 7)
+            {
+                grade += "+";
+            } 
+
+            if (score % 10 <= 3)
+            {
+                grade += "-";
+            }
+        }
+
+        else if (score >= 60 && score < 70)
+        {
+            grade = "D";
+
+            if (score % 10 >= 7)
+            {
+                grade += "+";
+            } 
+
+            if (score % 10 <= 3)
+            {
+                grade += "-";
+            }
+        }
+
+        else
+        {
+            grade = "F";
+        }
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, you have not passed. Good luck for your retake.");
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
     }
 }
