60aa55c8439185bd11b72b4a83c5ce41ebaa1dbd
dzowera <idzowera@byupathway.edu>
2025-04-04 00:10:57 +0200

    week 03

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -9,6 +9,67 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+
+        Bank myaccount = new Bank(0);
+
+        // deposit money
+        myaccount.Deposit(200);
+        System.Console.WriteLine($"The new deposited money is: {myaccount.GetBalance()}");
+
+        // withdraw money
+
+        myaccount.Withdraw(50);
+        System.Console.WriteLine($"Your current balance is: {myaccount.GetBalance()}");
+
+
+        // withdrwa more than available
+        myaccount.Withdraw(5000);
+        System.Console.WriteLine($"Your current balance is {myaccount.GetBalance()}");
+
+        myaccount._balance(500);
+
+
+
     }
-    
+
+    class Bank
+    {
+
+        private double _balance;
+        public Bank(double balance)
+        {
+            _balance = balance;
+        }
+
+        public void Deposit(double _deposit)
+        {
+            if (_deposit > 0)
+            {
+                _balance = _balance + _deposit;
+            }
+
+            else
+            {
+                System.Console.WriteLine($"You cannot deposit an amount less than 0");
+            }
+        }
+
+        public void Withdraw(double _withdraw)
+        {
+            if (_withdraw > 0 && _withdraw <= _balance)
+            {
+                _balance -= _withdraw;
+            }
+
+            else
+            {
+                System.Console.WriteLine("Invalid withdraw");
+            }
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
 }
--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,132 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference verseRef = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(verseRef, "For God so loved the world");
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            // Instead of Console.Clear(), we add many blank lines to simulate clearing the screen
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n");
+
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
+
+            string userInput = Console.ReadLine();
+            if (userInput.ToLower() == "quit") break;
+
+            scripture.HideRandomWords(2);
+        }
+
+        Console.WriteLine("\nAll words are hidden! Goodbye.");
     }
 }
+
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _isHidden ? "___" : _text;
+    }
+}
+
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_book} {_chapter}:{_verse}";
+    }
+}
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] wordsArray = text.Split(' ');
+        foreach (string word in wordsArray)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        int hiddenCount = 0;
+        while (hiddenCount < numberToHide)
+        {
+            int index = _random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+        foreach (Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+
+        return _reference.GetDisplayText() + " - " + string.Join(" ", displayWords);
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
